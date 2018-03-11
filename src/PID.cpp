6620a4ffdf990aaddb52c4d70ca92a61aaea7e28
Moritz <moritz.untersperger@gmail.com>
2018-03-11 14:50:18 +0100

    updated

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,11 +12,24 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	//setting coeffiecients of from arguments
+	this -> Kp = Kp;
+	this -> Ki = Ki;
+	this -> Kd = Kd;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	i_error += cte;
+	d_error = cte - p_error;
+	p_error = cte;
 }
 
 double PID::TotalError() {
+	double steering_angle = -Kp*p_error - Ki*i_error - Kd*d_error;
+	cout<< steering_angle<<endl;
+	return steering_angle;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-
+  double init_Kp = 0.2;
+  double init_Ki = 0.0;
+  double init_Kd = 2.2;
+  pid.Init(init_Kp, init_Ki, init_Kd);
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -57,6 +60,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
